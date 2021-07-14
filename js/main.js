9a88a13a53088273050de7fb08b0b325b1bd0838
zuramai <ahmadsaugigis@yahoo.com>
2021-07-14 12:23:02 +0700

    feat: left rotate

--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@ let avl = {
     left: null,
     right: null,
     height: 1,
+    circleEl: null,
+    textEl: null
 }
 
 const dx = 200
@@ -20,6 +22,8 @@ let viewBox = {
 
 let nodes = []
 let lines = []
+
+let rootIndex = 0
 
 let nodesEl = document.getElementById('nodes')
 let linesEl = document.getElementById('lines')
@@ -51,7 +55,8 @@ const randomId = () => Math.random().toString(36).substr(2, 5);
 const newNode = (number, parentId) => {
     let node = Object.assign({}, avl)
     node.parent = parentId
-    node.value = number
+    node.value = parseInt(number)
+    node.height = 1
     node.id = randomId()
 
     return node;
@@ -93,9 +98,154 @@ const createLine = (x1,y1,x2,y2) => {
     return line
 }
 
+
+const balanceFactor = (node) => {
+    if(!node) return 0
+    
+    return getHeight(node.left) - getHeight(node.right)
+}
+
+const max = (a,b) => a > b ? a : b
+
+const getHeight = (id) => {
+    let node = nodes[findNodeIndex(id)]
+    if(!node) return 0
+    return node.height
+}
+
+const getLine = (from,to) => {
+    console.log(`get line from ${from} to ${to}`)
+    let line =  lines.find(e => e.getAttribute('data-from') == from && e.getAttribute('data-to') == to)
+    console.log(lines)
+    console.log(`line exist: ${line}`)
+    return line
+}
+
+const leftRotate = (id) => {
+    let node = nodes[findNodeIndex(id)]
+    let nodeRight = nodes[findNodeIndex(node.right)]
+    let nodeRightLeft = nodes[findNodeIndex(nodeRight.left)]
+
+    nodeRight.left = node.id
+    node.right = nodeRightLeft.id
+
+    nodeRight.height = max(getHeight(nodeRight.left), getHeight(nodeRight.right)) + 1
+    node.height = max(getHeight(node.left), getHeight(node.right)) + 1
+
+    return nodeRight.id
+}
+
+const rightRotate = (id) => {
+    let node = nodes[findNodeIndex(id)]
+    let nodeLeft = nodes[findNodeIndex(node.left)]
+    let nodeLeftLeft = nodes[findNodeIndex(nodeLeft.left)]
+    let nodeLeftRight = nodes[findNodeIndex(nodeLeft.right)]
+    let nodeLeftLine = getLine(node.id, node.left);
+
+    nodeLeft.right = node.id
+    nodeLeft.parent = node.parent
+    node.parent = nodeLeft.id
+
+    node.left = nodeLeftRight ? nodeLeftRight.id : null
+
+    nodeLeft.height = max(getHeight(nodeLeft.left), getHeight(nodeLeft.right)) + 1
+    node.height = max(getHeight(nodeLeft.left), getHeight(nodeLeft.right)) + 1
+
+    nodeLeftLine.setAttribute('data-from', nodeLeft.id, node.id)
+
+    let nodeLeftLeftLine = getLine(nodeLeft.id, nodeLeft.left);
+    // change nodeleftleft position to the parent
+    nodeLeftLeft.cx = nodeLeft.cx
+    nodeLeftLeft.cy = nodeLeft.cy
+
+    // change nodeleft position to the parent
+    nodeLeft.cx = node.cx
+    nodeLeft.cy = node.cy
+
+    // change node position
+    node.cx = nodeLeft.cx + (nodeLeft.parent ? dxNext : dx)
+    node.cy = nodeLeft.cy + (nodeLeft.parent ? dyNext : dy)
+
+    let { x1, y1, x2, y2 } = getLineCoordinate(nodeLeftLeft)
+
+    // animate nodeLeft to its parent
+    gsap.to(nodeLeftLeftLine, {attr: { x1, y1, x2, y2 }, duration: 1, delay: 2, ease: 'power4.out'})
+    gsap.to(nodeLeft.circleEl, { attr: { cx: nodeLeft.cx, cy: nodeLeft.cy }, delay: 2, duration: 1, ease: 'power4.out' })
+    gsap.to(nodeLeft.textEl, { attr: { x: nodeLeft.cx, y: nodeLeft.cy }, delay: 2, duration: 1, ease: 'power4.out' })
+    
+    // animate nodeLeftLeft to its former nodeLeft position
+    gsap.to(nodeLeftLeft.circleEl, { attr: { cx: nodeLeftLeft.cx, cy: nodeLeftLeft.cy }, delay: 2, duration: 1, ease: 'power4.out' })
+    gsap.to(nodeLeftLeft.textEl, { attr: { x: nodeLeftLeft.cx, y: nodeLeftLeft.cy }, delay: 2, duration: 1, ease: 'power4.out' })
+
+    // animate nodeLeftLeft to its former nodeLeft position
+    let newline = getLineCoordinate(node)
+    gsap.to(nodeLeftLine, {attr: { x1: newline.x1, y1: newline.y1, x2: newline.x2, y2: newline.y2 }, duration: 1, delay: 2, ease: 'power4.out'})
+    gsap.to(node.circleEl, { attr: { cx: node.cx, cy: node.cy }, delay: 2, duration: 1, ease: 'power4.out' })
+    gsap.to(node.textEl, { attr: { x: node.cx, y: node.cy }, delay: 2, duration: 1, ease: 'power4.out' })
+
+    return nodeLeft.id
+}
+
+const highlightElement = (node) => {
+    node.circleEl.classList.add('active')
+    
+    setTimeout(() => {
+        node.circleEl.classList.remove('active')
+    })
+}
+
+const balance = (id) => {
+    console.log("balancing "+id)
+    let node = nodes[findNodeIndex(id)]
+    if(!node) return node
+
+    node.height = max(getHeight(node.left), getHeight(node.right))+1
+    console.log(`node ${id} height: ${node.height}, ${node.left}-${node.right}`)
+
+    let bf = balanceFactor(node)
+
+    if(bf > 1 && balanceFactor(node.left) >= 0) {
+        console.log("LL balancefactor: "+bf )
+        highlightElement(node)
+        return rightRotate(node.id)
+    }else if(bf > 1 && balanceFactor(node.left) < 0) {
+        console.log("LR balancefactor: "+bf )
+        highlightElement(node)
+        node.left = leftRotate(node.left)
+        return rightRotate(node.id)
+    }else if(bf < -1 && balanceFactor(node.right) <= 0) {
+        console.log("RR balancefactor: "+bf )
+        highlightElement(node)
+        return leftRotate(node.id)
+    }else if(bf < -1 && balanceFactor(node.right) > 0) {
+        console.log("RL balancefactor: "+bf )
+        highlightElement(node)
+        node.right = rightRotate(node.right)
+        return leftRotate(node.id)
+    }
+
+    
+
+    return node.id
+}
+
 const findNodeIndex = (id) => {
     let node = nodes.map((e) => e.id).indexOf(id)
     return node;
+}
+
+const getLineCoordinate = (node) => {
+    let parent = nodes[findNodeIndex(node.parent)]
+    let theta = 25
+    let x1r = circleDefaultAttrs.r * Math.cos(theta * (Math.PI/180))
+    let x2r = circleDefaultAttrs.r * Math.cos(theta * (Math.PI/180))
+
+    let x1 = node.value > parent.value ? parent.cx + x1r : parent.cx - x1r
+    let y1 = parent.cy + circleDefaultAttrs.r * Math.sin(theta * (Math.PI / 180))
+    let x2 = node.value > parent.value ? node.cx - x2r : node.cx + x2r
+    let y2 = node.cy - circleDefaultAttrs.r * Math.sin(30 * (Math.PI / 180))
+
+    return {x1, y1, x2, y2}
 }
 
 const drawNode = (nodeId) => {
@@ -108,8 +258,8 @@ const drawNode = (nodeId) => {
     group.classList.add(`node-${node.id}`)
 
     // if the parent id is root
-    let theDx = (parent && parent.id == nodes[0].id) ? dx : dxNext
-    let theDy = (parent && parent.id == nodes[0].id) ? dy : dyNext
+    let theDx = (parent && parent.id == nodes[rootIndex].id) ? dx : dxNext
+    let theDy = (parent && parent.id == nodes[rootIndex].id) ? dy : dyNext
 
     if(!parent) {
         // if no parent, then this is root
@@ -122,19 +272,13 @@ const drawNode = (nodeId) => {
 
     let circle = createCircle(node.cx, node.cy)
     if(parent) {
-        let theta = 25
-        let x1r = circleDefaultAttrs.r * Math.cos(theta * (Math.PI/180))
-        let x2r = circleDefaultAttrs.r * Math.cos(theta * (Math.PI/180))
-
-        let x1 = node.value > parent.value ? parent.cx + x1r : parent.cx - x1r
-        let y1 = parent.cy + circleDefaultAttrs.r * Math.sin(theta * (Math.PI / 180))
-        let x2 = node.value > parent.value ? node.cx - x2r : node.cx + x2r
-        let y2 = node.cy - circleDefaultAttrs.r * Math.sin(30 * (Math.PI / 180))
+        let {x1,y1,x2,y2} = getLineCoordinate(node)
         let line = createLine(x1,y1,x2,y2)
 
         line.setAttribute('data-from', parent.id)
         line.setAttribute('data-to', node.id)
         linesEl.appendChild(line)
+        lines.push(line)
 
         // animate the line
         gsap.fromTo(line, {attr: {x1,y1,x2:x1, y2:y1}}, {attr:{x1,y1,x2,y2}, duration: 1,  ease:"power4.out"})
@@ -145,8 +289,13 @@ const drawNode = (nodeId) => {
     group.appendChild(circle)
     group.appendChild(text)
     nodesEl.appendChild(group)
+    
+    node.circleEl = circle
+    node.textEl = text 
+
     gsap.fromTo(group, {opacity: 0}, {opacity:1, duration: 1, delay:1, ease:"power4.out"})
 }
+
 
 const createNode = (currentId, value, parentId) => {
     let currentNode = nodes[findNodeIndex(currentId)]
@@ -165,7 +314,8 @@ const createNode = (currentId, value, parentId) => {
     } else if (value < currentNode.value) {
         currentNode.left = createNode(currentNode.left, value, currentNode.id)
     }
-    return currentNode.id
+
+    return balance(currentId)
 }
 
 let form = document.getElementById('form')
@@ -191,7 +341,11 @@ form.addEventListener('submit', (e) => {
 
             break;
         case "insert":
-            createNode(nodes[0].id, value, nodes[0].id)
+            createNode(nodes[rootIndex].id, value, nodes[rootIndex].id)
+            
+            // set new root
+            rootIndex = nodes.findIndex(e => e.parent == null)
+            console.log('set root = '+rootIndex)
             console.log(nodes)
             break;
         default:
